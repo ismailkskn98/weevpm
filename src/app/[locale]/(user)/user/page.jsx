75ddaa9a4e31118f4b79b943f2ad2647d90e7acf
İsmail Keskin <hamdismailkskn98@gmail.com>
2025-07-08 16:58:12 +0300

    Yeni bağımlılık olarak @tabler/icons-react eklendi. Ayrıca, motion kütüphanesi güncellendi ve kullanıcı arayüzü bileşenlerinde önemli değişiklikler yapıldı. Kullanıcı arayüzü için yeni bir yan menü tasarımı eklendi ve kullanıcı deneyimini iyileştirmek için animasyonlar entegre edildi.

--- src/app/[locale]/(user)/user/page.jsx
+++ src/app/[locale]/(user)/user/page.jsx
@@ -4,6 +4,11 @@ export default function page() {
     return (
         <div>
             <h1>User Page</h1>
+            <section className='w-full h-[300px] flex items-center justify-between gap-8'>
+                <div className='w-full h-full flex-1 bg-teal flex items-center justify-between rounded-lg'></div>
+                <div className='w-full h-full flex-1 bg-green-800 flex items-center justify-between rounded-lg'></div>
+                <div className='w-full h-full flex-1 bg-green-300 flex items-center justify-between rounded-lg'></div>
+            </section>
         </div>
     )
 }
