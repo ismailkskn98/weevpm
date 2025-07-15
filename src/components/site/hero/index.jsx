6820e9669fdf43fbaa9c87c87da0961c111665c5
İsmail Keskin <hamdismailkskn98@gmail.com>
2025-07-15 08:32:38 +0300

    style: Güncellemeler yapıldı; hero bileşeninin stilinde ve animasyonlarında iyileştirmeler gerçekleştirildi. CSS dosyasında perspektif katmanlarının konumları ve animasyon süreleri optimize edildi.

--- src/components/site/hero/index.jsx
+++ src/components/site/hero/index.jsx
@@ -5,7 +5,7 @@ import HeroImage from "./heroImage";
 export default function Hero() {
   return (
     <section id="hero" className="fluid gridContainer w-full pt-32 mx-auto max-w-full xl:max-w-11/12 3xl:max-w-10/12">
-      <main className="w-full flex-col md:flex-row flex items-center justify-center md:justify-between gap-13 md:gap-1">
+      <main className="w-full max-w-full 2xl:max-w-11/12 mx-auto flex-col md:flex-row flex items-center justify-center md:justify-between gap-13 md:gap-1">
         <HeroContent />
         <HeroImage />
       </main>
